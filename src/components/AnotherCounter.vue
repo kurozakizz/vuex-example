18e4ee7bb50194a72f6f9dd3bfd497facfa08aa9
kurozakizz <squallcope@gmail.com>
2018-02-09 15:26:54 +0700

    Add vuex example project

---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="asyncIncrement({ by: 50, duration: 500 })">Async Increment</button>
+    <button class="btn btn-primary" @click="asyncDecrement({ by: 50, duration: 500 })">Async Decrement</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  methods: {
+    ...mapActions([
+      'asyncIncrement',
+      'asyncDecrement'
+    ])
+
+    // ...mapMutations([
+    //   'increment',
+    //   'decrement'
+    // ]),
+
+    // increment() {
+    //   this.$store.commit('increment')
+    // },
+    // decrement() {
+    //   this.$store.commit('decrement')
+    // }
+  }
+};
+</script>
